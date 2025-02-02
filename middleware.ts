6d6daf50e7b5f5cb53fd22058a08119a492a6406
Spend-Lytics <spendlytics1@gmail.com>
2025-02-02 14:36:58 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
-import { verifyToken } from "./utils/jwt"
+import { verifyToken } from "./lib/jwt.ts"
 
 export function middleware(request: NextRequest) {
   // Check if the request is for the dashboard
